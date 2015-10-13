a0c11f4056c4b4fa15331f64d689e792bd9cc26e
Troy Starwalt <troy.starwalt@gmail.com>
2015-10-12 20:53:19 -0400

    Added in form and flash

--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
 require 'active_record'
 require 'sinatra'
 require 'sinatra/activerecord'
-
+require 'pry'
+require 'sinatra/flash'
 require './models.rb'
 
-
-
+enable :sessions
 
 get "/" do
     params[:users] = User.all
     erb :index
+    
+end
+
+post "/signup" do
+	User.create(fname: params[:fname], lname: params[:lname], email: params[:uemail])
+	redirect to "/"
 end
